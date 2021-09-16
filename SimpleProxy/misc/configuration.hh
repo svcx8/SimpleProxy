2743192d6ab0a149ad46e17674e4375b11c99237
svcx8 <51900752+svcx8@users.noreply.github.com>
2021-09-16 19:59:25 +0800

    Add protected for singleton class.

--- SimpleProxy/misc/configuration.hh
+++ SimpleProxy/misc/configuration.hh
@@ -6,11 +6,15 @@
 #include <string>
 
 class Configuration : public Singleton<Configuration> {
+    friend Singleton;
+
 public:
-    Configuration();
     int port_ = 2333;
     bool enable_doh_ = false;
     std::string doh_server_{ "https://doh.pub/dns-query" };
+
+protected:
+    Configuration();
 };
 
 #endif // configuration.hh
